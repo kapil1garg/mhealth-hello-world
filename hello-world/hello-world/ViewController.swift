fdad1adf78cb7f334311259472c7796c5e1557a9
Kapil Arun Garg 3 <kgarg6@sapient.com>
2016-01-21 02:21:54 -0600

    initial commit of ios app

---
+++ hello-world/hello-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello-world
+//
+//  Created by Kapil Garg on 1/21/16.
+//  Copyright © 2016 Kapil Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
