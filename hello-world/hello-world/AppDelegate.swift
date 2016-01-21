d403386f411d98609792cfb361bc3c3b812fbebf
Kapil Arun Garg 3 <kgarg6@sapient.com>
2016-01-21 13:35:05 -0600

    added parse

--- hello-world/hello-world/AppDelegate.swift
+++ hello-world/hello-world/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios/guide#local-datastore
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("CrRUiTswM1AZQHyQLTJ8MPdeJia3W5BvUgtY15Ld",
+            clientKey: "wudPgEgT5PAmChzJZDELSbQlFbSIyNklFCC8fqu4")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
