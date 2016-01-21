86bf4f4ba15371aa5e77b9b949ac9cc26b90114e
Kapil Arun Garg 3 <kgarg6@sapient.com>
2016-01-21 16:57:05 -0600

    step tracking, push to DB, and hardcoded thresholding working

--- hello-world/hello-world/ViewController.swift
+++ hello-world/hello-world/ViewController.swift
@@ -8,19 +8,62 @@
 
 import UIKit
 import Parse
+import CoreMotion
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var numberSteps: UILabel!
     @IBOutlet weak var thresholdText: UILabel!
+    @IBOutlet weak var thresholdValue: UITextField!
+    
+    // create pedeometer object
+    let pedoMeter = CMPedometer()
+    
+    // step threshold
+    var stepThreshold = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            print("Object has been saved.")
+        
+        // set default threshold to 1000 steps
+        self.thresholdValue.text = String(stepThreshold)
+        
+        // create date objects used for selecting time
+        let cal = NSCalendar.currentCalendar()
+        let calendarComponents = cal.components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate: NSDate())
+        
+        let timeZone = NSTimeZone.systemTimeZone()
+        cal.timeZone = timeZone
+        
+        // set time to midnight (beginning of day)
+        calendarComponents.hour = 0
+        calendarComponents.minute = 0
+        calendarComponents.second = 0
+        
+        let beginningOfDay = cal.dateFromComponents(calendarComponents)
+        
+        // begin step tracking
+        if(CMPedometer.isStepCountingAvailable()) {
+            self.pedoMeter.startPedometerUpdatesFromDate(beginningOfDay!, withHandler: { data, error in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if (error == nil) {
+                        // update number of steps on screen
+                        self.numberSteps.text = "\(data!.numberOfSteps)"
+                        
+                        // post step value to parse
+                        let newStepCount = PFObject(className: "pedometer_data")
+                        newStepCount["steps"] = data?.numberOfSteps
+                        newStepCount.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+                            print("Object has been saved.")
+                        }
+                        
+                        // check to see if threshold is met
+                        if (data?.numberOfSteps as! Int > self.stepThreshold) {
+                            self.thresholdText.text = "World"
+                        }
+                    }
+                })
+            })
         }
     }
 
